7c0912ec8c8b2dfbc4385314e23b03238b5af2ef
luiz-divino <nandodivinodev@gmail.com>
2026-03-13 18:09:53 -0300

    feat(Home): :bricks: criar estrutura inicial para a pagina Home
    - adicionar grid layout com tailwindCss

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+
+export const Home = () => {
+  return (
+    <div>
+     <main className="w-full gap-4 mx-auto max-w-7xl">
+        <h1 className="text-2xl text-center">e-commerce</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5">
+          product cards
+        </div>
+     </main>
+    </div>
+  );
+};
