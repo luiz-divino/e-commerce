fabe88f228ea2f6b05f49e2c8b9689e8b0c906b8
luiz-divino <nandodivinodev@gmail.com>
2026-03-13 19:20:10 -0300

    feat: :sparkles: renderizar dinamicamente a lista de produtos no componente Card

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,12 @@
+import { Card } from "../../components/CardProduct";
 
 export const Home = () => {
   return (
     <div>
      <main className="w-full gap-4 mx-auto max-w-7xl">
         <h1 className="text-2xl text-center">e-commerce</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5">
-          product cards
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
+          <Card/>
         </div>
      </main>
     </div>
