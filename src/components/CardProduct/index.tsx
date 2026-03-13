e8f85b8b3d50774b05dd1cb1c3758594ffa83ebb
luiz-divino <nandodivinodev@gmail.com>
2026-03-13 18:34:28 -0300

    feat(Card): :satellite: implementar integracao de API com useEffect e useState

--- src/components/CardProduct/index.tsx
+++ src/components/CardProduct/index.tsx
@@ -1,17 +1,30 @@
+import { useState, useEffect } from "react";
+import { IProduct } from "../../interfaces/contract";
+import api from "../../api/api";
 
 export const Card = () => {
+  const [product, setProduct] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    async function getProduct() {
+      try {
+        const response = await api();
+        setProduct(response.products);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getProduct();
+  }, []);
+
   return (
-        <section className="w-full">
-            <img src="#" alt="#" />
-            <p>product name</p>
-        <div>
-            <strong>
-                product price
-            </strong>
-            <button>
-                add to cart
-            </button>
-        </div>
-        </section>
+    <section className="w-full">
+      <img src="#" alt="#" />
+      <p>product name</p>
+      <div>
+        <strong>product price</strong>
+        <button>add to cart</button>
+      </div>
+    </section>
   );
 };
