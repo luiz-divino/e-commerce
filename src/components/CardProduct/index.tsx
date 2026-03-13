ba069a78c2a69a9de3be3c8b0fbc4a8cc584b6dc
luiz-divino <nandodivinodev@gmail.com>
2026-03-13 17:54:02 -0300

    feat: :bricks: criar estrutura inicial para o component Card

---
+++ src/components/CardProduct/index.tsx
@@ -0,0 +1,17 @@
+
+export const Card = () => {
+  return (
+        <section className="w-full">
+            <img src="#" alt="#" />
+            <p>product name</p>
+        <div>
+            <strong>
+                product price
+            </strong>
+            <button>
+                add to cart
+            </button>
+        </div>
+        </section>
+  );
+};
