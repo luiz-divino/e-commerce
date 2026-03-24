faee1e0c3375f0d958e016caae93d891727acd9f
luizdivino <nandodivinodev@gmail.com>
2026-03-24 12:19:23 -0300

    feat: :sparkles: criar estrutura inicial da pagina carrinho de compras

---
+++ src/pages/Cart/index.tsx
@@ -0,0 +1,15 @@
+export const Cart = () => {
+    return(
+        <div>
+            <section>
+               <img src="" alt="" />
+               <strong>price</strong>
+               <div>
+                <button>+</button>
+                <span>quantity</span>
+                <button>-</button>
+               </div>
+            </section>
+        </div>
+    )
+}
