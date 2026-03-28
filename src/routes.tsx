7bb0535bf982589944fbfad50d424e98c8f67380
luizdivino <nandodivinodev@gmail.com>
2026-03-28 20:11:18 -0300

    refactor(layout): :recycle: mover Header para o roteador e evitar renderização duplicada na Home

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { Cart } from "./pages/Cart"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Cart } from "./pages/Cart";
+import { Header } from "./components/Header";
 
-export const Router = () =>{
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/cart" element={<Cart/>}/>
-            </Routes>
-        </BrowserRouter>
-    )
-}
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
