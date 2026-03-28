682607ccb6c006bfb582d8c019547c408641f805
luizdivino <nandodivinodev@gmail.com>
2026-03-28 20:20:19 -0300

    style: :lipstick: ajustar classes tailwind e espacamentos da interface do carrinho

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,15 +1,19 @@
 export const Cart = () => {
-    return(
-        <div>
-            <section>
-               <img src="" alt="" />
-               <strong>price</strong>
-               <div>
-                <button>+</button>
-                <span>quantity</span>
-                <button>-</button>
-               </div>
-            </section>
+  return (
+    <div className="w-full max-w-7xl mx-auto">
+      <section className="flex justify-between items-center mx-auto mt-10">
+        <img src="#" alt="" />
+
+        <strong>price</strong>
+
+        <div className="flex gap-3 items-center">
+          <button>+</button>
+          <span>quantity</span>
+          <button>-</button>
         </div>
-    )
-}
+
+        <strong className="float-right">Subtotal: value</strong>
+      </section>
+    </div>
+  );
+};
