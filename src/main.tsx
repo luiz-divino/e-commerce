3ca865d756463ae2fcce282a89f9e7230470f88d
Pedro-Nunes22 <pedrosoaresnunes2003@gmail.com>
2026-04-18 19:54:05 -0300

    feat: :sparkles: adicionar contextProvider para consumo global de estados

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { CartContextProvider } from "./contexts/CartContext/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <CartContextProvider>
+      <App />
+    </CartContextProvider>
   </StrictMode>,
-)
+);
